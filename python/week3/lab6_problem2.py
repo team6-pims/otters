897649b59cf8c32dce94b53ee50d93a1771b372e
team6-pims <44691409+team6-pims@users.noreply.github.com>
2018-11-01 21:27:44 -0700

    Add files via upload

---
+++ python/week3/lab6_problem2.py
@@ -0,0 +1,40 @@
+def getPic():
+  return makePicture(pickAFile())
+
+# Problem 2 - Posterization
+def Artify():
+  pic = getPic()
+  
+  for x in range(0, getWidth(pic)):
+    for y in range(0, getHeight(pic)):
+      # Get pixel details
+      p = getPixel(pic, x, y)
+      
+      # Get individual color values
+      r = getRed(p)
+      g = getGreen(p)
+      b = getBlue(p)
+      
+      # Run each color through our filter
+      r = posterizer(r)
+      g = posterizer(g)
+      b = posterizer(b)
+      
+      # Combine the values into a Color object
+      c = makeColor(r, g, b)
+      
+      # Change current pixel to new color
+      setColor(p, c)
+  show(pic)
+
+def posterizer(n):
+  if n < 64:
+    return 46
+  if n > 63 and n < 128:
+    return 95
+  if n > 127 and n < 192:
+    return 159
+  if n > 191 and n < 256:
+    return 223
+  return 0
+    
