2577bcf7b7bae516847bfc749be40b7b95e013cd
ialej001 <ivan.alejandre@gmail.com>
2018-12-05 23:03:59 -0800

    Update menu.py

--- python/final_project/menu.py
+++ python/final_project/menu.py
@@ -1,1 +1,11 @@
 # Command line menu placeholder
+
+# the other two .py files are going to be imported as modules
+# read https://docs.python.org/2/tutorial/modules.html to learn how
+
+import #pic functions
+import #songconverter
+
+def main():
+  # fill this stuff out, Matt
+  return
