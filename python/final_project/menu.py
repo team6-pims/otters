6b5afa01beea5b26d111305bc054e9ebf9c7f1df
matand2000 <44530791+matand2000@users.noreply.github.com>
2018-12-09 19:59:32 -0800

    Update menu.py

--- python/final_project/menu.py
+++ python/final_project/menu.py
@@ -1,11 +1,28 @@
-# Command line menu placeholder
 
-# the other two .py files are going to be imported as modules
-# read https://docs.python.org/2/tutorial/modules.html to learn how
-
-import #pic functions
-import #songconverter
-
-def main():
-  # fill this stuff out, Matt
-  return
+def mainMenu():
+  #provide user display information
+  openingStr = "Welcome to Manipulating Your Image.\nPlease enter your choice of option:\n"
+  openingStr1 = "1: Applying your choice of filter to an image\n"
+  openingStr2 = "2: Applying your choice of filter to images in a directory\n"
+  openingStr3 "3: Converting an image to sound\n"
+  exitStr = "Type in exit to quit the program"
+  stringTotal = openingStr + openingStr1 + openingStr2+ openingStr3 + exitStr
+  
+  errorStr = "You have not selected an option, try again"
+  
+  #get choice
+  choice = requestString(stringTotal)
+  print choice
+  
+  #check choice with menus
+  #currently fillers until other code has been added
+  if choice == 1:
+    return choice
+  elif choice == 2:
+    return choice
+  elif choice == 3:
+    return choice
+  else:
+    print errorStr
+    
+  
