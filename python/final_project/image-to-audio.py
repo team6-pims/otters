ab0d55c058603f4d57eb7df168dd49771a5df6b6
ialej001 <ivan.alejandre@gmail.com>
2018-12-10 20:55:44 -0800

    Update image-to-audio.py

--- python/final_project/image-to-audio.py
+++ python/final_project/image-to-audio.py
@@ -16,19 +16,19 @@ def convert_pic2audio(filePath):
   except:
     return -1
   
-  sndIdxArray = pixelToNotes(picIn)
+  soundValues = pixelToNotes(picIn)
    
   #all sound clips of same length
-  outputSong = makeEmptySound( len(sndIdxArray) * get_numSmpls() , get_clipSampleRate() )  
-  return assemble_ouputSong(sndIdxArray, outputSong)
+  outputSong = makeEmptySound( len(soundValues) * get_numSamples() , get_clipSampleRate() )  
+  return assemble_ouputSong(soundValues, outputSong)
   
   
 #
-# get_correctSound(sndIdx)
+# get_correctSound(soundIndex)
 #    returns sound clip corresponding to sndIdx
 #    should be updated to reflect your local machine
 #    path to sounds
-def get_correctSound(sndIdx):
+def get_correctSound(soundIndex):
   basePath = r"/home/captain/CSUMB/CST205/Final/Rawmusicnotes/"
   a = basePath + "clipped_A.wav"
   b = basePath + "clipped_B.wav"
@@ -37,30 +37,30 @@ def get_correctSound(sndIdx):
   e = basePath + "clipped_E.wav"
   f = basePath + "clipped_F.wav"
   g = basePath + "clipped_G.wav" 
-  if sndIdx == 0:
-    tmp = a
-  if sndIdx == 1:
-    tmp = b
-  if sndIdx == 2:
-    tmp =  c
-  if sndIdx == 3:
-    tmp =  d
-  if sndIdx == 4:
-    tmp =  e
-  if sndIdx == 5:
-    tmp =  f
-  if sndIdx == 6:
-    tmp =  g
-  return makeSound(tmp)
+  if soundIndex == 0:
+    temp = a
+  if soundIndex == 1:
+    temp = b
+  if soundIndex == 2:
+    temp =  c
+  if soundIndex == 3:
+    temp =  d
+  if soundIndex == 4:
+    temp =  e
+  if soundIndex == 5:
+    temp =  f
+  if soundIndex == 6:
+    temp =  g
+  return makeSound(temp)
 
 #
 # assemble_ouputSong()
 #     take in clip index list and output-empty-song obj
 #     returns assembled song
-def assemble_ouputSong(clpIdxLst, outputSong ):
-  for i in clpIdxLst:
-    currClp = get_correctSound(i)
-    outputSong = copy(currClp, outputSong)
+def assemble_ouputSong(clipIndexList, outputSong):
+  for i in clipIndexList:
+    currentClip = get_correctSound(i)
+    outputSong = copy(currentClip, outputSong)
   return outputSong 
 
 #
@@ -72,15 +72,15 @@ def get_clipSampleRate():
 #
 # get_numSmpls()
 #      get nbr of samples in a music clip
-def get_numSmpls():
+def get_numSamples():
   return getLength( makeSound(get_correctSound(0) ) )
 #
 #  copy()
 #    Source is the source sound (the short clip)
 #    Target is the target sound
 def copy(source , target, start = 0):  
-  sourLen = getLength(source)
-  for i in range(0 , sourLen ):
+  sourceLength = getLength(source)
+  for i in range(0, sourceLength):
     setSampleValueAt(target, start + i, getSampleValueAt(source, i))    
   return target
 
@@ -94,34 +94,43 @@ def copy(source , target, start = 0):
 # This function calls findShrinkFactor() to 'shrink' the image if need be. This is 
 # done to ensure the resulting "song" is not too long.
 
-def pixelToNotes(picture)
+def pixelToNotes(picture):
   # declare list to contain all note integers
   allNotes = list()
+  
   # threshold for picture size to be passed as an argument
   maxArea = 5000
   
+  # obtain dimensions of picture, and if odd, make even
+  picWidth = getWidth(picture)
+  picHeight = getHeight(picture)
+  if picWidth%2 == 1:
+    picWidth -= 1
+  if picHeight%2 == 1:
+    picHeight -= 1
+  
   # find the factor which is how big a step we're going to iterate through in the following
-  # nested loops
-  factor = findShrinkFactor(picture, maxArea, 1)
+  # recursion
+  factor = findShrinkFactor(picWidth, picHeight, maxArea, 1)
   
   # distance() works by calculating the distance between two colors as if they were
   # mapped on a 3 dimensional cartesian graph. We'll use the keyword 'black' to 
   # represent (0,0,0). The resulting number that distance returns is saved in rawNote
   # Then we take that variable and use modulus to get a number 0-6 and append that
   # number to a list.
-  for x in range(0, getWidth(picture), factor):
-    for y in range(0, getHeight(picture), factor):
+  for x in range(0, picWidth, factor):
+    for y in range(0, picHeight, factor):
       pixel = getPixel(picture, x, y) 
-      rawNote = distance(getColor(pixel), black)
+      rawNote = int(distance(getColor(pixel), black)*1000)
       note = rawNote%7
       allNotes.append(note)
   
   # return the list of numbers
   return allNotes
   
-#     shrinkPictureTosize(picture, maxArea, factor)
+#     findShrinkFactor(picWidth, picHeight, maxArea, factor)
 #
-# This function requires three arguments, a variable containing raw picture data
+# This function requires four arguments, height and width of a picture,
 # an integer for the maximum picture area and an integer that represents how many
 # pixels are to be skipped for each loop iteration. 
 # If the current picture area after being divided by the initial factor 1,
@@ -130,24 +139,20 @@ def pixelToNotes(picture)
 # to size. 
 # Returns: one integer to be used in parent function
 
-def findShrinkFactor(picture, maxArea, factor):
+def findShrinkFactor(picWidth, picHeight, maxArea, factor):
   # obtain dimensions, then consider odd lengthed pictures
-  picHeight = getHeight(picture) / factor
-  picWidth = getWidth(picture) / factor
-  
-  if picWidth%2 == 1:
-    picWidth -= 1
-  if picHeight%2 == 1:
-    picHeight -= 1
+  newPicHeight = picHeight / factor
+  newPicWidth = picWidth / factor
   
   # obtain current area
-  picArea = picHeight * picWidth
+  picArea = newPicHeight * newPicWidth
   
   # compare to threshold and either 'shrink' if the conditional evaluates true
   # if not, pass back current value for factor
-  if picArea > maxArea:
-    factor += 1
-    findShrinkFactor(workingPicture, maxArea, factor)
+  if (picArea < maxArea):
+    return factor - 1
+  else:
+    return findShrinkFactor(picWidth, picHeight, maxArea, factor + 1)
 
-  return factor
-
+def getPic():
+  return makePicture(pickAFile())
