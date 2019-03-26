a6dfb9c14a72b59f871f391df2e3297b993971f3
bit-flipping-alejo <44530684+bit-flipping-alejo@users.noreply.github.com>
2019-03-25 20:46:44 -0700

    Add Deck testing to Assig3
    Added a tested version of the deck output to assig3

--- CST338/Seperate files/assig3.java
+++ CST338/Seperate files/assig3.java
@@ -58,5 +58,57 @@ public class assig3 {
       }
       
       System.out.println("\nEmpty hand:\n" + hand.toString());
+      
+      
+      int numPacks = 2;
+      System.out.println();
+      System.out.println("Now we create a Deck with " + numPacks + " packs of card in it");
+      Deck myDeck = new Deck(numPacks);
+      
+      System.out.println("Now we deal all the cards");
+      
+      for (int i = 0; i < Deck.CARDS_PER_PACK * numPacks; i++) {
+         Card dealtCard = myDeck.dealCard();
+         System.out.println("Card" + i + ": " + dealtCard.toString());   
+      }
+      
+      System.out.println("End of dealing " + numPacks + "pack(s) of unshuffled cards");
+      System.out.println();
+      System.out.println("Re-Initializing Deck with " + numPacks + "packs of cards");
+      myDeck = new Deck(numPacks);
+      
+      System.out.println("Shuffing deck of cards");
+      myDeck.shuffle();
+      System.out.println("Dealing shuffled cards");
+      for (int i = 0; i < Deck.CARDS_PER_PACK * numPacks; i++) {
+         Card dealtCard = myDeck.dealCard();
+         System.out.println("Card" + i + ": " + dealtCard.toString());   
+      }
+      System.out.println("End of dealing " + numPacks + "pack(s) of shuffled cards");
+      System.out.println();
+      
+      System.out.println("Repeating above exercise with 1 pack of cards");
+      System.out.println("Instantiating single deck with default constructor");
+      myDeck = new Deck();
+      System.out.println("Dealing unshuffled cards");
+      for (int i = 0; i < Deck.CARDS_PER_PACK * 1; i++) {
+         Card dealtCard = myDeck.dealCard();
+         System.out.println("Card" + i + ": " + dealtCard.toString());   
+      }
+      System.out.println("End of dealing unshuffled single deck");
+      System.out.println("Re-instantiating single deck");
+      myDeck = new Deck();
+      System.out.println("Now shuffling the single deck");
+      myDeck.shuffle();
+      System.out.println("Deck is now shuffled");
+      System.out.println("Now dealing shuffled cards");
+      for (int i = 0; i < Deck.CARDS_PER_PACK * 1; i++) {
+         Card dealtCard = myDeck.dealCard();
+         System.out.println("Card" + i + ": " + dealtCard.toString());   
+      }
+      System.out.println("End of dealing shuffled cards");
+      
+      
+      
    }
 }
