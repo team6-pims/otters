72c73cf9c4a96529c7a346bda1466208518b8092
bit-flipping-alejo <44530684+bit-flipping-alejo@users.noreply.github.com>
2019-03-28 22:53:59 -0700

    added barcode image test class
    Im not sure if the prof wants this, but it wont hurt to show we tested it

--- CST338/Seperate files/Module4/Assig4.java
+++ CST338/Seperate files/Module4/Assig4.java
@@ -6,6 +6,57 @@ public class Assig4 {
 
     public static void main(String[] args)
     {
+      System.out.println("Now begin Barcode Image class test");
+      System.out.println("Test default constructor and print to screen");
+      BarcodeImage bc0 = new BarcodeImage();
+      
+      System.out.println();
+      bc0.displayToConsole();
+      System.out.println();
+      
+      String[] str = new String[3];
+      
+      str[0] = "101";
+      str[1] = "010";
+      str[2] = "101";
+      
+      System.out.println("Test string constructor");
+      BarcodeImage bc1 = new BarcodeImage(str);
+      BarcodeImage bc2 = new BarcodeImage(str);
+      
+      System.out.println("Test print to screen with str constructor");
+      bc1.displayToConsole();
+      
+      System.out.println("Test equals on 2 equivalent objects");
+      
+      System.out.println("BCI1 == BCI2? " + bc1.equals(bc2));
+      System.out.println();
+      
+      System.out.println("Now lets test setters");
+      System.out.println("Change first row to all 1s and second to all 0s");
+      
+      bc1.setPixel(0, 0, false);
+      bc1.setPixel(0, 2, false);
+      
+      bc1.setPixel(0, 0, true);
+      bc1.setPixel(0, 2, true);
+      
+      System.out.println("End");
+      System.out.println("Lets test");
+      bc1.displayToConsole();
+      
+      System.out.println("Now lets test getters");
+      System.out.println("Lets get bottom row");
+      System.out.println();
+      System.out.println(bc1.getPixel(2, 0));
+      System.out.println(bc1.getPixel(2, 1));
+      System.out.println(bc1.getPixel(2, 2));
+      System.out.println();
+      System.out.println("Success");
+      System.out.println("End Barcode Image class test");
+      System.out.println("Now begin Instructor defined test");  
+        
+        
        String[] sImageIn =
        {
           "                                               ",
