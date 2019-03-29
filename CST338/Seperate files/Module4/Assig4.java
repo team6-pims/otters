6987bb6e126e8b27cf27111f55d2073b76936f33
Conocolo <conocolo@gmail.com>
2019-03-28 20:56:39 -0700

    Added module 4

---
+++ CST338/Seperate files/Module4/Assig4.java
@@ -0,0 +1,70 @@
+public class Assig4 {
+
+    public static void main(String[] args)
+    {
+       String[] sImageIn =
+       {
+          "                                               ",
+          "                                               ",
+          "                                               ",
+          "     * * * * * * * * * * * * * * * * * * * * * ",
+          "     *                                       * ",
+          "     ****** **** ****** ******* ** *** *****   ",
+          "     *     *    ****************************** ",
+          "     * **    * *        **  *    * * *   *     ",
+          "     *   *    *  *****    *   * *   *  **  *** ",
+          "     *  **     * *** **   **  *    **  ***  *  ",
+          "     ***  * **   **  *   ****    *  *  ** * ** ",
+          "     *****  ***  *  * *   ** ** **  *   * *    ",
+          "     ***************************************** ",  
+          "                                               ",
+          "                                               ",
+          "                                               "
+ 
+       };      
+             
+          
+       
+       String[] sImageIn_2 =
+       {
+             "                                          ",
+             "                                          ",
+             "* * * * * * * * * * * * * * * * * * *     ",
+             "*                                    *    ",
+             "**** *** **   ***** ****   *********      ",
+             "* ************ ************ **********    ",
+             "** *      *    *  * * *         * *       ",
+             "***   *  *           * **    *      **    ",
+             "* ** * *  *   * * * **  *   ***   ***     ",
+             "* *           **    *****  *   **   **    ",
+             "****  *  * *  * **  ** *   ** *  * *      ",
+             "**************************************    ",
+             "                                          ",
+             "                                          ",
+             "                                          ",
+             "                                          "
+ 
+       };
+      
+       BarcodeImage bc = new BarcodeImage(sImageIn);
+       DataMatrix dm = new DataMatrix(bc);
+      
+       // First secret message
+       dm.translateImageToText();
+       dm.displayTextToConsole();
+       dm.displayImageToConsole();
+       
+       // second secret message
+       bc = new BarcodeImage(sImageIn_2);
+       dm.scan(bc);
+       dm.translateImageToText();
+       dm.displayTextToConsole();
+       dm.displayImageToConsole();
+       
+       // create your own message
+       dm.readText("What a great resume builder this is!");
+       dm.generateImageFromText();
+       dm.displayTextToConsole();
+       dm.displayImageToConsole();
+    }   
+}
---
+++ CST338/Seperate files/Module4/BarcodeIO.java
@@ -0,0 +1,17 @@
+/**
+ * BarcodeIO
+ */
+interface BarcodeIO {
+
+    public boolean scan(BarcodeImage bc);
+
+    public boolean readText(String text);
+
+    public boolean generateImageFromText();
+
+    public boolean translateImageToText();
+
+    public void displayTextToConsole();
+    
+    public void displayImagetoConsole();
+}
