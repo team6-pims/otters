89cca4a096dc98e1d05fe1b29ca46d8fc503bce6
ialej001 <ivan.alejandre@gmail.com>
2019-04-09 10:14:27 -0700

    Update Card.java
    formatting

--- CST338/Seperate files/Module5/Card.java
+++ CST338/Seperate files/Module5/Card.java
@@ -1,19 +1,14 @@
-
-
 public class Card {
-
    public enum Suit {
       CLUBS, DIAMONDS, HEARTS, SPADES;
    }
-
    public char value;
    public Suit suit;
    public boolean errorFlag = false;
-
-   public static char[] valuRanks = {'2', '3', '4', '5', '6', '7', '8', '9', 
+   public static char[] valueRanks = {'2', '3', '4', '5', '6', '7', '8', '9', 
          'T', 'J', 'Q', 'K', 'A' ,'X' }; 
 
-   /* Constructors */
+   /**Constructors */
    public Card(char value, Suit suit) {
       set(value, suit);
    }
@@ -22,12 +17,11 @@ public class Card {
       set('A', Suit.SPADES );
    }
 
-   public Card (Card card2copy) {
-      set( card2copy.value, card2copy.suit );
+   public Card (Card cardToCopy) {
+      set( cardToCopy.value, cardToCopy.suit );
    }
 
    // getters
-
    public char getValue() {
       return this.value;
    }
@@ -40,55 +34,54 @@ public class Card {
       return this.errorFlag;
    }
 
-   /* toString
+   /**toString
     * Return a string representation of class
     * or returns [Invaid Card] if errorFlag
     * is true*/
    public String toString() {
-      if ( errorFlag ) {
+      if (errorFlag) 
          return "[Invalid Card]";
-      } else {
+      else 
          return value + " of " + suit;
-      }
    }
 
-   /*set
+   /**set
     * setter for class variables calls
     * isValid to check for correctness of
     * inputs. If all valid, sets variables
     * if not, then does not set and sets errorFlag
     * to true*/
    public boolean set(char value, Suit suit) {
-
       value = Character.toUpperCase(value);
 
       if(isValid(value, suit)) {
          this.value = value;
          this.suit = suit;
          return true;
-      } else {
+      } 
+      else {
          this.errorFlag = true;
          return false;
       }
    }
 
 
-   /*equals
+   /**equals
     * Check for equality, true if equal
     * false if not*/
    public boolean equals( Card card ) {
-      return ( ( card.value == this.value ) && ( this.suit == card.suit  ) );
+      return ((card.value == this.value)&&(this.suit == card.suit));
    }
 
 
-   /*
+   /**
     * isValid 
-    *   Fcn only needs to validate card value,
+    *   Function only needs to validate card value,
     *   since input to calling functions take
     *   enum as input, it is automatically
     *   validated
     *   
-    *   input is expected to be a capitol character or number as a char
+    *   input is expected to be a capital character or number as a char
     *   */
    private boolean isValid (char value, Suit suit) {
       char[] acceptableCards = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 
@@ -101,58 +94,53 @@ public class Card {
       return false;   
    }
 
-   /*THIS IS A BIG ASSUMPTION
+   /**THIS IS A BIG ASSUMPTION
     * I ASSUME THIS IS A SAFE BY REFERENCE PASSING
     * I.E. THE VOID DOES NOT RETURN A SORTED ARRAY
     * BUT INSTEAD SORTS THE CARD[] ARRAY PASSED IN*/
-   static void arraySort(Card[] ary2sort, int arraySize) {
-
-      // per prof spades, hearts, diamonds, clubs is the order of sorting
-
+   static void arraySort(Card[] arrayToSort, int arraySize) {
+      // per professor: spades, hearts, diamonds, clubs is the order of sorting
       boolean arraySorted = false;
-
 
       while (!arraySorted) {
          arraySorted = true;
-         for (int i = 0; i < ( ary2sort.length - 1 ); i++) {
-            if (suitGreaterThan(ary2sort[i].suit, ary2sort[i + 1].suit)) {
+         for (int i = 0; i < (arrayToSort.length - 1); i++) {
+            if (suitGreaterThan(arrayToSort[i].suit, arrayToSort[i + 1].suit)) {
                // if suit is greater then we 
                // must swap
-               Card tmp = new Card(ary2sort[i + 1]); // yay copy constructor
-               ary2sort[i + 1] = ary2sort[i];
-               ary2sort[i] = tmp;
+               Card tmp = new Card(arrayToSort[i + 1]); //copy constructor
+               arrayToSort[i + 1] = arrayToSort[i];
+               arrayToSort[i] = tmp;
                arraySorted = false;
-            } else {
+            } 
+            else {
                // if suit is not greater than
                // we must check value
-               if (ary2sort[i].suit == ary2sort[i + 1].suit && 
-                     (valueGreaterThan( ary2sort[i].value, ary2sort[i + 1].value ))) {
-                  Card tmp = new Card(ary2sort[i + 1]); // yay copy constructor
-                  ary2sort[i + 1] = ary2sort[i];
-                  ary2sort[i] = tmp;
+               if ((arrayToSort[i].suit == arrayToSort[i + 1].suit) && 
+                     (valueGreaterThan(arrayToSort[i].value, 
+                           arrayToSort[i + 1].value ))) {
+                  Card tmp = new Card(arrayToSort[i + 1]); //copy constructor
+                  arrayToSort[i + 1] = arrayToSort[i];
+                  arrayToSort[i] = tmp;
                   arraySorted = false;
                } 
             }
-
          }
 
          if (arraySorted) {
             break;
          }
-
       }
-
-
    }
 
-   /*return card1 > card2*/
+   /**return card1 > card2*/
    public static boolean cardGreaterThan(Card card1, Card card2) {
       boolean suitbool = suitGreaterThan(card1.suit, card2.suit);
       boolean valbool = valueGreaterThan(card1.value, card2.value);
       return suitbool || valbool;
    }
 
-   /*suitGreaterThan
+   /**suitGreaterThan
     * helper function for arraySort
     * returns suit1 > suit2 as per prof instructions
     * in order of: spades, hearts, diamonds, clubs*/
@@ -193,13 +181,10 @@ public class Card {
       default:
          suit2val = 0; // will never happen, compiler complained   
       }
-
       return (suit1val > suit2val);
-
-
    }
 
-   /*valueGreaterThan
+   /**valueGreaterThan
     * helper function for arraySort
     * returns val1 > val2 as per prof instructions
     * in order of: valuRanks*/
@@ -207,32 +192,19 @@ public class Card {
       int val1int = -1;
       int val2int = -1;
       
-      for (int i = 0; i < valuRanks.length; i++) {
-         if (valuRanks[i] == val1) {
+      for (int i = 0; i < valueRanks.length; i++) {
+         if (valueRanks[i] == val1) {
             val1int = i;
             break;
          }
       }
 
-      for (int i = 0; i < valuRanks.length; i++) {
-         if (valuRanks[i] == val2) {
+      for (int i = 0; i < valueRanks.length; i++) {
+         if (valueRanks[i] == val2) {
             val2int = i;
             break;
          }
       }
-
-      
       return (val1int > val2int);
-      
-      
    }
-
 }
-
-
-
-
-
-
-
-
