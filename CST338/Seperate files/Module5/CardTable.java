05128ca83d456263a171d1503f4f2b46a549fa63
ialej001 <ivan.alejandre@gmail.com>
2019-04-09 09:43:46 -0700

    Update CardTable.java
    formatting

--- CST338/Seperate files/Module5/CardTable.java
+++ CST338/Seperate files/Module5/CardTable.java
@@ -4,28 +4,21 @@ import javax.swing.*;
 public class CardTable extends JFrame{
    static int MAX_CARDS_PER_HAND = 56;
    static int MAX_PLAYERS = 2;  // for now, we only allow 2 person games
-
    private int numCardsPerHand;
    private int numPlayers;
-
    public JPanel pnlComputerHand, pnlHumanHand, pnlPlayArea;
 
    public CardTable(String title, int NumCardsPerHand, int NumPlayers)  {
-      if (!(NumCardsPerHand > 0 && NumCardsPerHand < MAX_CARDS_PER_HAND)) {
+      if (!(NumCardsPerHand > 0 && NumCardsPerHand < MAX_CARDS_PER_HAND)) 
          numCardsPerHand = MAX_CARDS_PER_HAND;;
 
-      }
-
-      if (!(NumPlayers > 0 && NumPlayers < MAX_PLAYERS)) {
+      if (!(NumPlayers > 0 && NumPlayers < MAX_PLAYERS))
          numPlayers = MAX_PLAYERS;
-      }
+      
       super()
       numCardsPerHand = NumCardsPerHand;
       numPlayers = NumPlayers;
-
    }
-
-   
    
    /*Getters*/
    public int getNumCardsPerHand() {
@@ -39,21 +32,18 @@ public class CardTable extends JFrame{
    /*Setters
     * Probably not needed?*/
    public boolean setNumCardsPerHand(int newNum) {
-      if (!(numCardsPerHand > 0 && numCardsPerHand < MAX_CARDS_PER_HAND)) {
+      if (!(numCardsPerHand > 0 && numCardsPerHand < MAX_CARDS_PER_HAND))
          return false;
-      }
+      
       numCardsPerHand = newNum;
       return true;
    }
 
    public boolean setNumPlayers(int newNum) {
-      if (!(numPlayers > 0 && numPlayers < MAX_PLAYERS)) {
+      if (!(numPlayers > 0 && numPlayers < MAX_PLAYERS))
          return false;
-      }
+      
       numPlayers = newNum;
       return true;
    }
-   
-   
-   
 }
