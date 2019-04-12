2f13c7dbefcab0519f122cde580f8bbe0a82653b
bit-flipping-alejo <44530684+bit-flipping-alejo@users.noreply.github.com>
2019-04-11 20:38:39 -0700

    Add files via upload

---
+++ CST338/Seperate files/Module6/GUIController.java
@@ -0,0 +1,14 @@
+package hw6;
+/*Controllers act as an interface between Model 
+ * and View components to process all the business 
+ * logic and incoming requests, manipulate data 
+ * using the Model component and interact with 
+ * the Views to render the final output. For 
+ * example, the Customer controller will handle 
+ * all the interactions and inputs from the Customer 
+ * View and update the database using the Customer 
+ * Model. The same controller will be used to view 
+ * the Customer data.*/
+public class GUIController {
+
+}
---
+++ CST338/Seperate files/Module6/GUIView.java
@@ -0,0 +1,10 @@
+package hw6;
+/*The View component is used for 
+ * all the UI logic of the application. 
+ * For example, the Customer view will 
+ * include all the UI components such as 
+ * text boxes, dropdowns, etc. that the 
+ * final user interacts with.*/
+public class GUIView {
+
+}
